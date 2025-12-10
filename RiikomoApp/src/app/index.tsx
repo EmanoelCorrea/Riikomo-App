a4d8c48116c75bc994180daefc157b8c6f1a8f0e
EmanoelDev <161870258+EmanoelCorrea@users.noreply.github.com>
2025-12-10 23:49:41 +0000

    Primeiro commit: criando o HelloWorld.

---
+++ RiikomoApp/src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+import { style } from './styles'
+
+export default function AppRiikomo(){
+    return(
+        <View style={style.conteiner}>
+            <Text style={style.text}>
+                Hello,World!!!
+            </Text>
+        </View>
+    )
+}
---
+++ RiikomoApp/src/app/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native"
+
+export const style = StyleSheet.create({
+    conteiner:{
+        flex:1,
+        backgroundColor:'#0fff',
+        justifyContent:'center',
+        alignItems:'center'
+    
+    },
+    text:{
+        color:'#000',
+        textAlign:'center',
+        fontSize:30,
+    }    
+})
